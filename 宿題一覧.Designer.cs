49281bff7ad6060dfda74a794fe6c03b0221a254
NagatoSun-0729 <67617364+NagatoSun-0729@users.noreply.github.com>
2020-08-11 14:31:46 +0900

    Add files via upload

---
+++ 宿題一覧.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace HL_塾管理
+{
+    partial class 宿題一覧
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(宿題一覧));
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_宿題追加 = new System.Windows.Forms.Button();
+            this.btn_宿題分配 = new System.Windows.Forms.Button();
+            this.gv_homework = new System.Windows.Forms.DataGridView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.宿題番号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.宿題名 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.言語 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.予定完成時間 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.宿題内容 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gv_homework)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_search
+            // 
+            this.txt_search.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txt_search.Location = new System.Drawing.Point(40, 40);
+            this.txt_search.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(265, 36);
+            this.txt_search.TabIndex = 3;
+            this.txt_search.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(348, 39);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 36);
+            this.btn_search.TabIndex = 4;
+            this.btn_search.Text = "検索";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // btn_宿題追加
+            // 
+            this.btn_宿題追加.Location = new System.Drawing.Point(585, 42);
+            this.btn_宿題追加.Name = "btn_宿題追加";
+            this.btn_宿題追加.Size = new System.Drawing.Size(107, 34);
+            this.btn_宿題追加.TabIndex = 5;
+            this.btn_宿題追加.Text = "課題追加";
+            this.btn_宿題追加.UseVisualStyleBackColor = true;
+            this.btn_宿題追加.Click += new System.EventHandler(this.btn_宿題追加_Click);
+            // 
+            // btn_宿題分配
+            // 
+            this.btn_宿題分配.Location = new System.Drawing.Point(765, 42);
+            this.btn_宿題分配.Name = "btn_宿題分配";
+            this.btn_宿題分配.Size = new System.Drawing.Size(87, 34);
+            this.btn_宿題分配.TabIndex = 6;
+            this.btn_宿題分配.Text = "課題分配";
+            this.btn_宿題分配.UseVisualStyleBackColor = true;
+            this.btn_宿題分配.Click += new System.EventHandler(this.btn_宿題分配_Click);
+            // 
+            // gv_homework
+            // 
+            this.gv_homework.AllowUserToAddRows = false;
+            this.gv_homework.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gv_homework.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.gv_homework.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv_homework.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.宿題番号,
+            this.宿題名,
+            this.言語,
+            this.予定完成時間,
+            this.宿題内容});
+            this.gv_homework.Location = new System.Drawing.Point(40, 104);
+            this.gv_homework.Name = "gv_homework";
+            this.gv_homework.RowTemplate.Height = 24;
+            this.gv_homework.Size = new System.Drawing.Size(872, 415);
+            this.gv_homework.TabIndex = 7;
+            this.gv_homework.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.gv_homework_CellBeginEdit);
+            this.gv_homework.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.gv_homework_CellEndEdit);
+            this.gv_homework.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gv_homework_CellValueChanged);
+            this.gv_homework.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.gv_homework_DataError);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 521);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(962, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // 宿題番号
+            // 
+            this.宿題番号.HeaderText = "課題番号";
+            this.宿題番号.Name = "宿題番号";
+            this.宿題番号.ReadOnly = true;
+            // 
+            // 宿題名
+            // 
+            this.宿題名.HeaderText = "課題名";
+            this.宿題名.Name = "宿題名";
+            this.宿題名.ReadOnly = true;
+            this.宿題名.Width = 120;
+            // 
+            // 言語
+            // 
+            this.言語.HeaderText = "言語";
+            this.言語.Name = "言語";
+            this.言語.ReadOnly = true;
+            this.言語.Width = 70;
+            // 
+            // 予定完成時間
+            // 
+            this.予定完成時間.HeaderText = "予定完成時間";
+            this.予定完成時間.Name = "予定完成時間";
+            this.予定完成時間.Width = 130;
+            // 
+            // 宿題内容
+            // 
+            this.宿題内容.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Blue;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Blue;
+            this.宿題内容.DefaultCellStyle = dataGridViewCellStyle2;
+            this.宿題内容.HeaderText = "課題内容";
+            this.宿題内容.Name = "宿題内容";
+            // 
+            // 宿題一覧
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 543);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.gv_homework);
+            this.Controls.Add(this.btn_宿題分配);
+            this.Controls.Add(this.btn_宿題追加);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_search);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "宿題一覧";
+            this.Text = "課題一覧";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.宿題一覧_FormClosed);
+            this.Load += new System.EventHandler(this.宿題一覧_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gv_homework)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_宿題追加;
+        private System.Windows.Forms.Button btn_宿題分配;
+        private System.Windows.Forms.DataGridView gv_homework;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 宿題番号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 宿題名;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 言語;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 予定完成時間;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 宿題内容;
+    }
+}
---
+++ 宿題履歴.Designer.cs
@@ -0,0 +1,132 @@
+﻿using System.Windows.Forms;
+
+namespace HL_塾管理
+{
+    partial class 宿題履歴
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_st = new System.Windows.Forms.DataGridView();
+            this.lbl_stcode = new System.Windows.Forms.Label();
+            this.lbl_stname = new System.Windows.Forms.Label();
+            this.lbl_件数 = new System.Windows.Forms.Label();
+            this.lbl_code = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_st)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_st
+            // 
+            this.dgv_st.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_st.Location = new System.Drawing.Point(54, 172);
+            this.dgv_st.Name = "dgv_st";
+            this.dgv_st.RowTemplate.Height = 24;
+            this.dgv_st.Size = new System.Drawing.Size(631, 415);
+            this.dgv_st.TabIndex = 4;
+            // 
+            // lbl_stcode
+            // 
+            this.lbl_stcode.AutoSize = true;
+            this.lbl_stcode.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lbl_stcode.Location = new System.Drawing.Point(476, 94);
+            this.lbl_stcode.Name = "lbl_stcode";
+            this.lbl_stcode.Size = new System.Drawing.Size(99, 20);
+            this.lbl_stcode.TabIndex = 6;
+            this.lbl_stcode.Text = "学生番号：";
+            // 
+            // lbl_stname
+            // 
+            this.lbl_stname.AutoSize = true;
+            this.lbl_stname.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lbl_stname.Location = new System.Drawing.Point(104, 94);
+            this.lbl_stname.Name = "lbl_stname";
+            this.lbl_stname.Size = new System.Drawing.Size(99, 20);
+            this.lbl_stname.TabIndex = 5;
+            this.lbl_stname.Text = "学生名前：";
+            // 
+            // lbl_件数
+            // 
+            this.lbl_件数.AutoSize = true;
+            this.lbl_件数.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lbl_件数.Location = new System.Drawing.Point(50, 606);
+            this.lbl_件数.Name = "lbl_件数";
+            this.lbl_件数.Size = new System.Drawing.Size(39, 20);
+            this.lbl_件数.TabIndex = 7;
+            this.lbl_件数.Text = "0件";
+            // 
+            // lbl_code
+            // 
+            this.lbl_code.AutoSize = true;
+            this.lbl_code.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lbl_code.Location = new System.Drawing.Point(581, 94);
+            this.lbl_code.Name = "lbl_code";
+            this.lbl_code.Size = new System.Drawing.Size(29, 20);
+            this.lbl_code.TabIndex = 8;
+            this.lbl_code.Text = "01";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lbl_name.Location = new System.Drawing.Point(209, 94);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(39, 20);
+            this.lbl_name.TabIndex = 9;
+            this.lbl_name.Text = "123";
+            // 
+            // 宿題履歴
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 635);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.lbl_code);
+            this.Controls.Add(this.lbl_件数);
+            this.Controls.Add(this.lbl_stcode);
+            this.Controls.Add(this.lbl_stname);
+            this.Controls.Add(this.dgv_st);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Name = "宿題履歴";
+            this.Text = "宿題履歴";
+            this.Load += new System.EventHandler(this.宿題履歴_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_st)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_st;
+        private System.Windows.Forms.Label lbl_stcode;
+        private System.Windows.Forms.Label lbl_stname;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private Label lbl_件数;
+        private Label lbl_code;
+        private Label lbl_name;
+    }
+}
